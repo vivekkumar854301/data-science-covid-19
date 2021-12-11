17ad3d7f64b94b10d7bc57520b2976ee8d2d68bd
vivek kumar <vivekkumar2020@vitbhopal.ac.in>
2021-12-12 01:25:03 +0530

    u

--- contact for asistance.php
+++ contact for asistance.php
@@ -7,7 +7,7 @@ $message= $_POST['message'];
 $to = "viveksingh144399@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message ."\r\n mobile number=" .$number;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
+$headers = "From: noreply@covid-19.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
