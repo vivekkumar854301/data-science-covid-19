ee55416f1fcbcea57f4ffde37355dc396f3ef12b
vivek kumar <vivekkumar2020@vitbhopal.ac.in>
2022-01-23 00:51:17 +0530

    upload
    upload in github

---
+++ New folder/datewise.php
@@ -0,0 +1,25 @@
+<?php
+
+$data = file_get_contents('https://api.covid19india.org/data.json');
+$coranalive = json_decode($data, true);
+
+$statescount = count($coranalive['statewise']);
+
+ //<!-- If you like my videos then plz subscribe and leave a comment on the video about your experience ok   -->
+
+
+$i=1;
+while($i < $statescount){
+
+  echo $coranalive['statewise'][$i]['lastupdatedtime'] . "<br>"  ;
+  echo $coranalive['statewise'][$i]['state'] . "<br>"  ;
+  echo $coranalive['statewise'][$i]['confirmed'] . "<br>"  ;
+  echo $coranalive['statewise'][$i]['active'] . "<br>"  ;
+  echo $coranalive['statewise'][$i]['recovered'] . "<br>"  ;
+  echo $coranalive['statewise'][$i]['deaths'] . "<br>"  ; 
+
+  $i++;
+}
+
+
+?>
